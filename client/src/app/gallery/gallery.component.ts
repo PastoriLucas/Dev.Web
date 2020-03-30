f038cba5104d2d2c8d33b9a16ad4e3c4a2047a42
Matthieu Luyckx <he201687@students.ephec.be>
2020-03-30 16:05:21 +0200

    Toolbar + Galerie interactive
    Remise de la toolbar en forme dans le but de créer des listes déroulantes (e.g pour la galerie)

--- client/src/app/gallery/gallery.component.ts
+++ client/src/app/gallery/gallery.component.ts
@@ -9,7 +9,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 export class GalleryComponent implements OnInit {
 
-  public image = [];
+  public transition;
 
   constructor(private http: HttpClient) {
   }
@@ -22,25 +22,8 @@ export class GalleryComponent implements OnInit {
       headers
     })
       .subscribe(result => {
-        // @ts-ignore
-        console.log(result.length);
-        // tslint:disable-next-line:prefer-for-of no-unused-expression
-        // @ts-ignore
-        // tslint:disable-next-line:prefer-for-of
-        for (let i = 0; i < result.length; i++) {
-          console.log(result[i].image);
-          if ( i === 0) {
-            this.image[0] = result[0].image;
-          } else {
-            this.image[i] += result[i].image;
-          }
-          console.log(this.image);
-        }
-        const img = document.getElementsByClassName('imgs');
-        for (let r = this.image.length; r < img.length; r++ ) {
-          // @ts-ignore
-          img[r].style = 'display: none';
-        }
+        console.log(result);
+        this.transition = result;
       });
   }
 }
