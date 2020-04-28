1e2e7e5fa554bf3111599d9a050326778a6f3502
Pastori <he201669@students.ephec.be>
2020-04-28 16:28:49 +0200

    création de la page contact html
    html de la page contact

--- client/src/app/contact/contact.component.ts
+++ client/src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -8,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 
 export class ContactComponent implements OnInit {
 
-  constructor() {
-  }
+  public mailForm;
 
+  constructor(public http: HttpClient, private formBuilder: FormBuilder) {
+    this.mailForm = this.formBuilder.group({
+      formName : '',
+      formMail : '',
+      formSjt : '',
+      formMsg : ''
+    });
+  }
   ngOnInit(): void {
+  }
+  onSubmit(res) {
+    const headers = new HttpHeaders()
+      .set('Authorization', 'my-auth-token')
+      .set('Content-Type', 'application/json');
+    this.http.post('http://127.0.0.1:8888/contact', '', {
+      params : {
+        name : res.formName,
+        envoyeur : res.formMail,
+        objet : res.formSjt,
+        texte : res.formMsg
+      },
+      headers : headers
+    })
+      .subscribe(result => {
+        if (result === false) {
+          console.log('Mail not ok');
+        } else {
+          console.log('Mail ok');
+        }
+      });
   }
 }
