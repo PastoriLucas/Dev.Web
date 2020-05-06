d95f3a85b7e584feef22fe246a171b42808b8815
Pastori <he201669@students.ephec.be>
2020-05-06 11:24:35 +0200

    .Pas: Auto stash before merge of "Luc.Pas" and "origin/Luc.Pas"

--- client/login.js
+++ client/login.js
@@ -13,23 +13,23 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 
 app.all("/*", function(req, res, next){
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-	next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
 });
 
 //connection avec la db
 let pool = new pg.Pool({
-	user: 'lucaspasto',
-	host: '51.178.40.75',
-	database: 'ValouKervyn',
-	password: 'Magnetique',
-	port: '5432'
+  user: 'lucaspasto',
+  host: '51.178.40.75',
+  database: 'ValouKervyn',
+  password: 'Magnetique',
+  port: '5432'
 });
 pool.connect(function (err) {
-	if (err) throw err;
-	console.log("Connected!");
+  if (err) throw err;
+  console.log("Connected!");
 });
 // Password encryption for DB
 const bcrypt = require('bcrypt');
@@ -56,36 +56,36 @@ app.use(passport.session());
 ///////////////////// ROUTAGE /////////////////////
 
 app.post('/evenement',urlencodedParser, async (req, res) => {
-	// recupere les valeurs du formulaire
-	let sql = 'SELECT "eventId" , name, to_char("dateBegin", \'DD/MM/YYYY\') as "dateBegin", to_char("dateEnd", \'DD/MM/YYYY\') as "dateEnd", place, description, image from events ORDER BY "events"."dateBegin"';
-	await pool.query(sql, (err, rows) => {
-		return res.json(rows.rows);
-	});
+  // recupere les valeurs du formulaire
+  let sql = 'SELECT "eventId" , name, to_char("dateBegin", \'DD/MM/YYYY\') as "dateBegin", to_char("dateEnd", \'DD/MM/YYYY\') as "dateEnd", place, description, image from events ORDER BY "events"."dateBegin"';
+  await pool.query(sql, (err, rows) => {
+    return res.json(rows.rows);
+  });
 });
 
 app.post('/galerie',urlencodedParser, async (req, res) => {
-	let sql = 'SELECT id, name, size, to_char(creationdate, \'DD/MM/YYYY\') as creationdate, image, likes FROM paintings ORDER BY id';
-	await pool.query(sql, (err, rows) => {
-		return res.json(rows.rows);
-	});
+  let sql = 'SELECT id, name, size, to_char(creationdate, \'DD/MM/YYYY\') as creationdate, image, likes FROM paintings ORDER BY id';
+  await pool.query(sql, (err, rows) => {
+    return res.json(rows.rows);
+  });
 });
 
 app.post('/test',urlencodedParser, async (req, res) => {
-	let sql = 'SELECT * FROM users WHERE mail=\''+ req.query.email + '\'';
-	await pool.query(sql, (err,rows) => {
+  let sql = 'SELECT * FROM users WHERE mail=\''+ req.query.email + '\'';
+  await pool.query(sql, (err,rows) => {
     console.log(rows.rows);
-		if(rows.rows.length > 0) {
-			bcrypt.compare(req.query.password, rows.rows[0].password, (err, match) => {
-			  if (match){
+    if(rows.rows.length > 0) {
+      bcrypt.compare(req.query.password, rows.rows[0].password, (err, match) => {
+        if (match){
           req.login(rows.rows[0].id, function () {
             return res.end(true);
           });
-			  }
-			  return res.end(false);
+        }
+        return res.end(false);
       });
-		}
-		return res.end(false);
-	});
+    }
+    return res.end(false);
+  });
 });
 
 app.post('/new', [
@@ -135,58 +135,58 @@ app.post('/register',urlencodedParser, async (req, res) => {
     res.send(false);
   }
   let sql = 'SELECT * FROM users WHERE "mail"=\''+ req.query.email + '\' AND password =\''+ req.query.password + '\'';
-	await pool.query(sql, (err,rows) => {
-		if(err || rows.rows.length === 0) {
-			console.log('You can create user');
-			let notification = false;
-			if (req.query.notification === 'yes') notification = true;
-			bcrypt.genSalt(saltRounds, function(err, salt) {
-				bcrypt.hash(req.query.password, salt, function(err, hash) {
-					// Store hash in your password DB.
-					sql = 'INSERT INTO users (firstname, lastname, mail, password, notifications) ' +
-						'VALUES (\''+ req.query.firstname + '\', \''+ req.query.lastname+ '\', \''+ req.query.email+ '\', \''+ hash + '\', \''+notification+ '\')';
-					pool.query(sql, (err) => {
-						if (err) throw err;
-						return res.send(true);
-					});
-				});
-			});
-		} else {return res.send(false); }
-	});
+  await pool.query(sql, (err,rows) => {
+    if(err || rows.rows.length === 0) {
+      console.log('You can create user');
+      let notification = false;
+      if (req.query.notification === 'yes') notification = true;
+      bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(req.query.password, salt, function(err, hash) {
+          // Store hash in your password DB.
+          sql = 'INSERT INTO users (firstname, lastname, mail, password, notifications) ' +
+            'VALUES (\''+ req.query.firstname + '\', \''+ req.query.lastname+ '\', \''+ req.query.email+ '\', \''+ hash + '\', \''+notification+ '\')';
+          pool.query(sql, (err) => {
+            if (err) throw err;
+            return res.send(true);
+          });
+        });
+      });
+    } else {return res.send(false); }
+  });
 });
 
 app.post('/adminImg', urlencodedParser, (req) => {
 
-	let file = req.query.imageFile;
-	let newPath = 'C:/Users/natha/Documents/web/client/src/assets/img/'+ file;
-	file = 'C:\\Users\\natha\\OneDrive\\Images\\'+ file;
+  let file = req.query.imageFile;
+  let newPath = 'C:/Users/natha/Documents/web/client/src/assets/img/'+ file;
+  file = 'C:\\Users\\natha\\OneDrive\\Images\\'+ file;
 
-	fs.copyFile(file, newPath, (err)=> {
-		if (err) throw err;
-	});
-	file = '../../assets/img/' +req.query.imageFile;
-	let sql = "INSERT INTO paintings (name, size, creationdate, image) VALUES ('"+req.query.imageName+"', '"+req.query.imageSize+"', current_date, '"+file+"')";
-	pool.query(sql, (err) => {
-	  console.log('sent '+ err);
-		if (err) return false;
-	});
-	return true;
+  fs.copyFile(file, newPath, (err)=> {
+    if (err) throw err;
+  });
+  file = '../../assets/img/' +req.query.imageFile;
+  let sql = "INSERT INTO paintings (name, size, creationdate, image) VALUES ('"+req.query.imageName+"', '"+req.query.imageSize+"', current_date, '"+file+"')";
+  pool.query(sql, (err) => {
+    console.log('sent '+ err);
+    if (err) return false;
+  });
+  return true;
 });
 
 app.post('/adminEvent', urlencodedParser, (req) => {
 
-	let file = req.query.imageFile;
-	let newPath = 'C:/Users/natha/Documents/web/client/src/assets/img/'+ file;
-	file = 'C:\\Users\\natha\\OneDrive\\Images\\'+ file;
+  let file = req.query.imageFile;
+  let newPath = 'C:/Users/natha/Documents/web/client/src/assets/img/'+ file;
+  file = 'C:\\Users\\natha\\OneDrive\\Images\\'+ file;
 
-	console.log(file);
-	fs.rename(file, newPath);
-	file = '../../assets/img/' +req.query.imageFile;
-	let sql = "INSERT INTO event (name, size, creationdate, image) VALUES ('"+req.query.imageName+"', '"+req.query.imageSize+"', current_date, '"+file+"')";
-	pool.query(sql, (err, rows) => {
-		if (err) {return err}
-		return rows;
-	})
+  console.log(file);
+  fs.rename(file, newPath);
+  file = '../../assets/img/' +req.query.imageFile;
+  let sql = "INSERT INTO event (name, size, creationdate, image) VALUES ('"+req.query.imageName+"', '"+req.query.imageSize+"', current_date, '"+file+"')";
+  pool.query(sql, (err, rows) => {
+    if (err) {return err}
+    return rows;
+  })
 });
 
 app.post('/like', urlencodedParser, (req) => {
@@ -197,7 +197,7 @@ passport.serializeUser(function (user_id, done) {
   done(null, user_id);
 });
 passport.deserializeUser(function (user_id, done) {
-    done(null, user_id);
+  done(null, user_id);
 });
 
 
@@ -211,4 +211,3 @@ passport.deserializeUser(function (user_id, done) {
 //ecoute sur le port 8888
 app.listen(8888);
 //httpsServer.listen(8888);
-
