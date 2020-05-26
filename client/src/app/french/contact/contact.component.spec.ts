e102da52e5d18cd608e0df414f30ba8e96914b2d
Matthieu Luyckx <he201687@students.ephec.be>
2020-05-26 11:29:34 +0200

    Tests Contact + correction tests events

--- client/src/app/french/contact/contact.component.spec.ts
+++ client/src/app/french/contact/contact.component.spec.ts
@@ -2,25 +2,33 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FrContactComponent } from './contact.component';
 import {RouterTestingModule} from '@angular/router/testing';
-import {HttpClient, HttpHandler} from '@angular/common/http';
 import {FormBuilder} from '@angular/forms';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('FrContactComponent', () => {
   let component: FrContactComponent;
-  let fixture: ComponentFixture<FrContactComponent>;
+  let httpTestingController: HttpTestingController;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FrContactComponent ],
-      imports: [ RouterTestingModule],
-      providers: [HttpHandler, HttpClient, FormBuilder]
-    })
-    .compileComponents();
-  }));
+  const valeurTest = {formName: 'TestName', formMail: 'test@email.com', formSjt: 'testSubject', formMsg: 'testMessage'};
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FrContactComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      providers: [ FrContactComponent, FormBuilder ],
+      imports: [HttpClientTestingModule, RouterTestingModule]
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
+    component = TestBed.inject(FrContactComponent);
+  });
+
+  afterEach(() => httpTestingController.verify());
+
+  it('Should do a post request to backend for sending email', () => {
+    const url = `http://51.178.40.75:8888/api/contact?name=TestName&envoyeur=test@email.com&objet=testSubject&texte=testMessage`;
+    component.onSubmit(valeurTest);
+    const req = httpTestingController.expectOne(url);
+    expect(req.request.urlWithParams).toBe(url);
+    expect(req.request.method).toBe('POST');
+    // expect(req.request.body).toEqual({message: 'User not found'});
+    req.flush({});
   });
 });
