57dfd4c847cbe1cbab4660bb12252bf5c6080b6e
Debongnie <he201699@students.ephec.be>
2020-05-26 15:13:11 +0200

    Google maps Evenement
    EventId

--- client/src/app/french/eventid/eventid.component.ts
+++ client/src/app/french/eventid/eventid.component.ts
@@ -1,9 +1,7 @@
-import {Component, OnInit, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders } from '@angular/common/http';
 import {CookieService} from 'ngx-cookie-service';
 import {FormBuilder} from '@angular/forms';
-
-
 
 @Component({
   selector: 'app-event',
@@ -45,8 +43,8 @@ export class FrEventidComponent implements OnInit {
           const page = window.location.pathname.split('/').pop();
           // tslint:disable-next-line:radix
           if (result[i].eventId.toString() === page) {
-            // this.place = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyCq4TvPKogUIilCh_38VgrV4URD5o1a8xk&q=' + result[i].place;
-            // document.getElementById('map').setAttribute('src', this.place);
+            this.place = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyCq4TvPKogUIilCh_38VgrV4URD5o1a8xk&q=' + result[i].place;
+            document.getElementById('map').setAttribute('src', this.place);
             this.event = result[i];
             this.image = '../../assets' + result[i].image;
             document.getElementById('image').setAttribute('src', this.image);
